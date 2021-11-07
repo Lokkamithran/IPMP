d1d0835ad913eb354728dd3c38a7d1e95b37fef7
Lokkamithran <76836427+Lokkamithran@users.noreply.github.com>
2021-11-07 19:51:09 +0530

    Create question11.cpp

---
+++ Week 1 Codes/question11.cpp
@@ -0,0 +1,34 @@
+//Program to find the Longest Increasing Subsequence
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void longestIncSubsequence(int *a, int size){
+  int i, j, length = 1;
+  vector<int> pos(size, 1);
+
+  for(i = 1; i < size; i++)
+    for(j = 0; j < i; j++)
+      if(a[i] > a[j] && pos[i] == pos[j])
+          pos[i]++;
+
+  for(i = 0; i < size; i++)
+    if(pos[i] > length)
+      length = pos[i];
+
+  cout << endl << "The length of the Longest Increasing Subsequence is: "
+      << length << endl;
+}
+
+int main(){
+  int n;
+  cout << "Enter the size of the array: ";
+  cin >> n;
+  int a[n];
+  cout << "Enter the array: ";
+  for(int i = 0; i < n; i++)
+    cin >> a[i];
+
+  longestIncSubsequence(a, n);
+  return 0;
+}
